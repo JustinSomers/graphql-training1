64851226d9b753c8dbdf02cbfff91478fe732312
kylebanner <kyle.banner2@nutrien.com>
2022-04-16 15:21:02 -0600

    Add linter, fix lint rules

--- monolith/server.ts
+++ monolith/server.ts
@@ -3,21 +3,21 @@ import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-(async function () {
-const schema = await loadSchema('**/*.graphql', {
-  loaders: [new GraphQLFileLoader()]
-});
-const server = new ApolloServer({
-  schema,
-});
+(async () => {
+  const schema = await loadSchema('**/*.graphql', {
+    loaders: [new GraphQLFileLoader()],
+  });
+  const server = new ApolloServer({
+    schema,
+  });
 
-const port = 4000;
-const app = express();
-await server.start();
+  const port = 4000;
+  const app = express();
+  await server.start();
 
-server.applyMiddleware({ app });
-app.listen({ port }, () => {
-  // eslint-disable-next-line no-console
-  console.log(`🚀 Apollo Server v3 GraphQL API launched at: http://localhost:${port}${server.graphqlPath}`);
-});
+  server.applyMiddleware({ app });
+  app.listen({ port }, () => {
+    // eslint-disable-next-line no-console
+    console.log(`🚀 Apollo Server v3 GraphQL API launched at: http://localhost:${port}${server.graphqlPath}`);
+  });
 })();
