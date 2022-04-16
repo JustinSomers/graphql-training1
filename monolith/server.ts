4ac8097e3f4a491278964b8b9fdc50d33f02c002
kylebanner <kyle.banner2@nutrien.com>
2022-04-16 15:43:00 -0600

    Update monolith server

--- monolith/server.ts
+++ monolith/server.ts
@@ -1,23 +1,27 @@
 import { loadSchema } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { createComplexityLimitRule } from 'graphql-validation-complexity';
+import config from './apolloServerConfig.json';
+import express from 'express';
 
 (async () => {
+  const app = express();
+
   const schema = await loadSchema('**/*.graphql', {
     loaders: [new GraphQLFileLoader()],
   });
-  const server = new ApolloServer({
+  const apolloServer = new ApolloServer({
     schema,
+    introspection: config.introspection,
+    validationRules: [createComplexityLimitRule(config.operationComplexityLimit)],
   });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, path: config.graphQLPath, });
 
-  const port = 4000;
-  const app = express();
-  await server.start();
-
-  server.applyMiddleware({ app });
+  const port = config.port;
   app.listen({ port }, () => {
     // eslint-disable-next-line no-console
-    console.log(`🚀 Apollo Server v3 GraphQL API launched at: http://localhost:${port}${server.graphqlPath}`);
+    console.log(`🚀 Apollo Server v3 GraphQL API launched at: http://localhost:${port}${apolloServer.graphqlPath}`);
   });
 })();
