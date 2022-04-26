50fa87e98e6ae219984a12fb1abd16c62eea5b4a
kylebanner <kyle.banner2@nutrien.com>
2022-04-25 20:20:15 -0600

    Update redis client console

--- monolith/src/redisClient.ts
+++ monolith/src/redisClient.ts
@@ -4,6 +4,7 @@ export type RedisClientType = ReturnType<typeof createClient>
 
 export async function redisClient(): Promise<RedisClientType> {
   const client: RedisClientType = createClient();
+  // eslint-disable-next-line no-console
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
   return client;
