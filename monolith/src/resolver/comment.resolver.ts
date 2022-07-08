81c38d3cf6570d8ae4346fadc880a48c8838193a
kylebanner <kyle.banner2@nutrien.com>
2022-07-08 11:45:50 -0600

    Add eslint comments

--- monolith/src/resolver/comment.resolver.ts
+++ monolith/src/resolver/comment.resolver.ts
@@ -6,7 +6,6 @@ import comments from '@monolith/service/comment/comment.service';
 
 const resolver: Resolvers = {
   Account: {
-    // eslint-disable-next-line max-len
     comments: async (_parent, _args, context: Context): Promise<Comment[]> => comments(context),
   },
 };
--- monolith/src/service/comment/comment.service.ts
+++ monolith/src/service/comment/comment.service.ts
@@ -7,6 +7,7 @@ import commentMapper from '@monolith/service/comment/comment.mapper';
 const comments = async (
   context: Context,
 ): Promise<Comment[]> => {
+  // eslint-disable-next-line max-len
   const commentsResponse = await context.dataSources.imgurApi.getComments(context.username);
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
